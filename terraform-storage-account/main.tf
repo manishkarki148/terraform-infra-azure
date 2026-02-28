5efd36e8c852cb08da1685cf597a58f315da7aaf
Manish Karki <90179912+manishkarki148@users.noreply.github.com>
2026-02-28 19:31:06 +1000

    Added modules for each subscription

--- terraform-storage-account/main.tf
+++ terraform-storage-account/main.tf
@@ -1,5 +1,23 @@
-module "my_storage" {
+module "my_storage_1" {
   source = "./modules/mk-storage-account"
+
+  providers = {
+    azurerm = azurerm.subscription_1
+  }
+
+  name                     = var.name
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = var.account_tier
+  account_replication_type = var.account_replication_type
+}
+
+module "my_storage_2" {
+  source = "./modules/mk-storage-account"
+
+  providers = {
+    azurerm = azurerm.subscription_2
+  }
 
   name                     = var.name
   resource_group_name      = var.resource_group_name
